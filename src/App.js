1e67d71c3735f791a0134ceb87865f2213319f5b
offmak <levyvitor606@gmail.com>
2022-10-13 08:48:03 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import Spline from '@splinetool/react-spline';
+
+export default function App() {
+  return (
+    <Spline scene="https://prod.spline.design/awxXsqzZ-2fa3aAN/scene.splinecode" />
+  );
+}
